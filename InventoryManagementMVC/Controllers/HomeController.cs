e196a5ab6d63f29c63188c0d53cea256307b5959
Joachim <joac3146@zbc.dk>
2021-04-13 13:26:44 +0200

    Added some SQL commands and an object class

--- InventoryManagementMVC/Controllers/HomeController.cs
+++ InventoryManagementMVC/Controllers/HomeController.cs
@@ -6,21 +6,46 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace InventoryManagementMVC.Controllers
 {
     public class HomeController : Controller
     {
+        SqlCommand command = new SqlCommand();
+        SqlDataReader dataReader;
+        SqlConnection connection = new SqlConnection();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            connection.ConnectionString = Properties.Resources.ConnectionString;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        private void FetchData()
+        {
+            try
+            {
+                connection.Open();
+                command.Connection = connection;
+
+                //+ Where User = User
+                command.CommandText = "SELECT [Section], [Row], [Drawer], [Room], [Product], [Amount], [Type], [Supplier], [SupplierNumber], [Description] FROM Inventory";
+                dataReader = command.ExecuteReader();
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public IActionResult Jokke()
--- InventoryManagementMVC/Models/Item.cs
+++ InventoryManagementMVC/Models/Item.cs
@@ -8,10 +8,141 @@ namespace InventoryManagementMVC.Models
 {
     public class Item
     {
-        [Key]
-        public int Id { get; set; }
+        //private string _user, Add this later
+        private string _section;
+        private string _row;
+        private string _drawer;
+        private string _room;
+        private string _product;
+        private int _amount;
+        private string _type;
+        private string _supplier;
+        private string _supplierNumber;
+        private string _description;
 
-        public string Borrower { get; set; }
+        public string Section
+        {
+            get
+            {
+                return this._section;
+            }
+            set
+            {
+                this._section = value;
+            }
+        }
+        public string Row
+        {
+            get
+            {
+                return this._row;
+            }
+            set
+            {
+                this._row = value;
+            }
+        }
+        public string Drawer
+        {
+            get
+            {
+                return this._drawer;
+            }
+            set
+            {
+                this._drawer = value;
+            }
+        }
+        public string Room
+        {
+            get
+            {
+                return this._room;
+            }
+            set
+            {
+                this._room = value;
+            }
+        }
+        public string Product
+        {
+            get
+            {
+                return this._product;
+            }
+            set
+            {
+                this._product = value;
+            }
+        }
+        public int Amount
+        {
+            get
+            {
+                return this._amount;
+            }
+            set
+            {
+                this._amount = value;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return this._type;
+            }
+            set
+            {
+                this._type = value;
+            }
+        }
+        public string Supplier
+        {
+            get
+            {
+                return this._supplier;
+            }
+            set
+            {
+                this._supplier = value;
+            }
+        }
+        public string SupplierNumber
+        {
+            get
+            {
+                return this._supplierNumber;
+            }
+            set
+            {
+                this._supplierNumber = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return this._description;
+            }
+            set
+            {
+                this._description = value;
+            }
+        }
 
+        public Item(string section, string row, string drawer, string room, string product, int amount, string type, string supplier, string supplierNumber, string description)
+        {
+            Section = section;
+            Row = row;
+            Drawer = drawer;
+            Room = room;
+            Product = product;
+            Amount = amount;
+            Type = type;
+            Supplier = supplier;
+            SupplierNumber = supplierNumber;
+            Description = description;
+        }
     }
 }
